6a5cb86705c39b56050523141f34597e6ab3e483
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2020-07-16 18:50:57 +0000

    Update meeting-agenda.md

--- include/BluJoy.h
+++ include/BluJoy.h
@@ -29,4 +29,5 @@ class BluJoy {
     float axis_states_raw[6] = { 0.0, 0.0, 0.0, 0.0, -1.0, -1.0 };
 
     float axis_min[6] = { -1.0, -1.0, -1.0, -1.0, -1.0, -1.0 };
-    float axis_mid[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 
+    float axis_mid[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+    float axis_max[6] = { 1.0, 1.

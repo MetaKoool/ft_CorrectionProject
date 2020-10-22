60e802da915637d5c5a27787b82d2de3d3fcf284
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2020-10-22 10:50:43 +0000

    updated plugins

--- include/BluJoy.h
+++ include/BluJoy.h
@@ -86,4 +86,4 @@ class BluJoy {
 
     void set_button_state (byte btn, byte state);
     void set_dpad_state(byte dpad_up, byte dpad_right, byte dpad_down, byte dpad_left);
-    void set_dpad_analog_state(byte axis_x, 
+    void set_dpad_analog_state(byte axis_x, byte axis_y, float value_x, f

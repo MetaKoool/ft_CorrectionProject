2dec34f5c7108d94e4c4fe1a7e28c1a679f28aab
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-04-08 18:50:39 +0000

    Login Page

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -21,4 +21,4 @@ void BluJoy::prefs_init () {
 
 void BluJoy::read_calibration_data () {
   for (byte axis = 0; axis < AXIS_COUNT; axis++) {
-    axis_min[axis] = preferences.getFl
+    axis_min[axis] = preferences.getFloat(axis_min_names[axis], -

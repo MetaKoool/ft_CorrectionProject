d8606e3894f2809e47bfd01889b18f82c116861e
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-04-15 15:43:44 +0000

    Removed commented-out code

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -29,4 +29,5 @@ void BluJoy::read_calibration_data () {
 
 void BluJoy::write_calibration_data () {
   for (byte axis = 0; axis < BluJoy::AXIS_COUNT; axis++) {
-    preferences.putFloat(axis_min_names[axis], a
+    preferences.putFloat(axis_min_names[axis], axis_min[axis]);
+    prefer

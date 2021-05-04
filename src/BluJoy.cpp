2b5782869bb0c763cf60b0d5be087d86e4770558
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-05-04 22:53:37 +0000

    Update QUICKSTART.md

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -31,4 +31,8 @@ void BluJoy::write_calibration_data () {
   for (byte axis = 0; axis < BluJoy::AXIS_COUNT; axis++) {
     preferences.putFloat(axis_min_names[axis], axis_min[axis]);
     preferences.putFloat(axis_mid_names[axis], axis_mid[axis]);
-    preferences.putFloat(axis_max_names[axis], axis_max[ax
+    preferences.putFloat(axis_max_names[axis], axis_max[axis]);
+  }
+}
+
+// https://git

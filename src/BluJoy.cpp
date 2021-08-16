ae5fc0d7c27253c08b1c7f686396c58e50bb05f6
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-08-16 18:53:58 +0000

    make file

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -80,4 +80,4 @@ void BluJoy::configure_gamepad () {
   ble_gamepad_cfg.setIncludeRyAxis(true);
 
   ble_gamepad_cfg.setAxesMin(ble_axis_min); // 0 --> int16_t - 16 bit signed integer - Can be in decimal or hexadecimal
-  ble_gamepad_cfg.setAxesMax(ble_axis_max); // 32767 --> int16_t - 16 bi
+  ble_gamepad_cfg.setAxesMax(ble_axis_max); // 32767 --> int16_t - 16 bit signed integer - Can be i

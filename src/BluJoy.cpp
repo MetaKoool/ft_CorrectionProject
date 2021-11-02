72f277993824e63de958e5309d26c29e0e120566
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-11-02 10:44:05 +0000

    deployment

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -93,4 +93,4 @@ float BluJoy::clean_axis_value (byte axis, float state) {
   float state_calibrated = 0.0;
 
   if (state > axis_mid[axis]) {
-    state_calibrated = map_range(state, 
+    state_calibrated = map_range(state, axis_mid[axis], axis_max[axis],

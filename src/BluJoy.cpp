c6bd67fb804d0a54f7274949b73862d6be11b9fc
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2021-11-23 14:22:53 +0000

    Create .bashrc

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -97,3 +97,5 @@ float BluJoy::clean_axis_value (byte axis, float state) {
   } else if (state < axis_mid[axis]) {
     state_calibrated = map_range(state, axis_min[axis], axis_mid[axis], -1.0, 0.0);
   }
+
+  return dz_scaled_radial(s

ebc0ebbb779e3991f2787f4768526cf0d3c703a3
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2022-05-10 07:11:43 +0000

    sched: change cpu_avg_load_per_task using h_nr_running

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -173,4 +173,5 @@ void BluJoy::set_dpad_state (byte dpad_up, byte dpad_right, byte dpad_down, byte
   } else if (dpad_down == LOW) {
     if (dpad_right == LOW) {
       dpad_state = DPAD_DOWN_RIGHT;
-    } else if (dpad_left
+    } else if (dpad_left == LOW) {
+      dpad_state 

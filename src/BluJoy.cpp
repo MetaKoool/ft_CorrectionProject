dbd03541a69368220852e71ed50f7ee40e197439
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2022-06-25 22:45:35 +0000

    update sync

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -204,4 +204,4 @@ void BluJoy::set_dpad_analog_state (byte axis_x, byte axis_y, float value_x, flo
   if (x > 0.0) {
     if (x > slope_y) dpad_right = LOW;
   } else if (x < 0.0) {
-    if (x 
+    if (x < slope_y) dpad_left = L

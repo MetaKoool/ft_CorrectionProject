cb3aee4e63e85b4fed23aee69bb5d709e7ef4cb2
MetaKoool <97041708+MetaKoool@users.noreply.github.com>
2022-09-24 20:37:49 +0000

    [wpe] bump to latest version

--- src/BluJoy.cpp
+++ src/BluJoy.cpp
@@ -270,4 +270,5 @@ void BluJoy::report () {
     if (button_states[btn] == LOW) {
       if (is_special) ble_gamepad.pressSpecialButton(ble_button);
       else ble_gamepad.press(ble_button);
-    } 
+    } else {
+      if (is_speci
